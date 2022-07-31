71a154ca0ca2773d0044b82cb6b425f6ac85cd31
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-07-31 12:32:05 +0530

    Update Root.py

--- Root.py
+++ Root.py
@@ -1,16 +1,17 @@
-import math
+import cmath
 a=eval(input('Enter the coeficient of x^2 : '))
 b=eval(input('Enter the coeficient of x : '))
 c=eval(input('Enter the coeficient of x^0 : '))
 d=b**2-(4*a*c)
-r=eval(math.sqrt(d))
+r=cmath.sqrt(d)
+print(r)
 if(d>0):
-	s=(-b+r)/4*a*c
-	t=(-b-r)/4*a*c
+	s=(-b+r)/(2*a)
+	t=(-b-r)/(2*a)
 elif(d==0):
-	t=s=-b/4*a*c
+	t=s=-b/2*a
 else:
-	s=eval((-b+r)/4*a*c)
-	t=eval((-b-r)/4*a*c)
+	s=eval((-b+r)/2*a)
+	t=eval((-b-r)/2*a)
 print('The roots are : ',s,' ',t)
 
