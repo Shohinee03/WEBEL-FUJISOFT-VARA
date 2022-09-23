14bd55de41c4be9620b9fc8651c9236c16b86929
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 23:16:01 +0530

    Create ass5.py

---
+++ Assignment-8/ass5.py
@@ -0,0 +1,5 @@
+# Write a python script to count elements in a tuple.
+test_list = [(2, 4), (6, 7), (5, 1), (6, 10), (8, 7)]
+x = [j for i in enumerate(test_list) for j in i]
+print(test_list)
+print(len(x))
