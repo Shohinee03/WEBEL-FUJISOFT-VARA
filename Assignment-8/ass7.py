ebd6fbc02c3755228ba0a1723484a86c6a286901
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 23:18:00 +0530

    Create ass7.py

---
+++ Assignment-8/ass7.py
@@ -0,0 +1,13 @@
+# Write a python script to create tuples with homogeneous elements from a tuple containing
+# heterogeneous elements.
+Tuple1 = (0, 1, 2, 3)
+Tuple2 = ('Now good', 'is', 'Google')
+ 
+Tuple3 = Tuple1 + Tuple2
+print("Tuple 1: ")
+print(Tuple1)
+print("\nTuple2: ")
+print(Tuple2)
+
+print("\nTuples after Concatenation: ")
+print(Tuple3)
