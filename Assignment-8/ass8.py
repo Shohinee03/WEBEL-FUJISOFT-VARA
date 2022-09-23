44f2115c04bda7bf8cd4d99bb5a1c2ace03394bd
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 23:20:19 +0530

    Update ass8.py

--- Assignment-8/ass8.py
+++ Assignment-8/ass8.py
@@ -1,5 +1,9 @@
 # Write a python script to compare two tuples, whether they contains the same elements in
 # any order or not.
-list1 = [10, 20, 4, 45, 99]
-list1.sort()
-print("Largest element is:", list1[-1])
+test_tup1 = (10, 4, 5)
+test_tup2 = (13, 5, 18)
+
+print("The original tuple 1 : " + str(test_tup1))
+print("The original tuple 2 : " + str(test_tup2))
+res = all(x < y for x, y in zip(test_tup1, test_tup2))
+print("Are all elements in second tuple greater than first ? : " + str(res))
