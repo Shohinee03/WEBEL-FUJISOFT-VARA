70c466923cb0d8d41e4ee2557176acdf630770b5
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 22:54:52 +0530

    Create ass1.py

---
+++ Assignment-7/ass1.py
@@ -0,0 +1,6 @@
+# Write a python script to calculate length of the string. String is given by user.
+str = input("Enter a string: ")
+a = 0
+for s in str:
+      a = a+1
+print("Length of the input string is:", a)
