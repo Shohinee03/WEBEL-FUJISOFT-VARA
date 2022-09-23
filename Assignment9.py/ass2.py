d24f64065caea3136a0a4a051b401f30e8adbb23
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 23:40:21 +0530

    Update ass2.py

--- Assignment9.py/ass2.py
+++ Assignment9.py/ass2.py
@@ -1,17 +1,15 @@
-from math import *
-def Probability(sum, times) :
-    favorable, total, probability = 0.0, 36.0, 0
-    for i in range(7) :
-        for j in range(7) :
-            if ((i + j) == sum) :
-                favorable += 1
- 
-    gcd1 = gcd(int(favorable), int(total))
-    favorable = favorable / gcd1
-    total = total / gcd1
-    probability = pow(total, times)
- 
-    return int(probability)
-if __name__ == "__main__" :
-    sum, times = 7, 7
-    print("1","/",Probability(sum, times))
+numr=int(input("Enter range:"))
+
+print("Prime numbers:",end=' ')
+
+for n in range(1,numr):
+
+    for i in range(2,n):
+
+        if(n%i==0):
+
+            break
+
+    else:
+
+        print(n,end=' ')  
