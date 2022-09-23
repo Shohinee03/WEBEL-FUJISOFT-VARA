3c9e35ff0f68b2d1c26d5238fa1056b8c243307d
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 23:19:32 +0530

    Create ass8.py

---
+++ Assignment-8/ass8.py
@@ -0,0 +1,5 @@
+# Write a python script to compare two tuples, whether they contains the same elements in
+# any order or not.
+list1 = [10, 20, 4, 45, 99]
+list1.sort()
+print("Largest element is:", list1[-1])
