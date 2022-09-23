83d0bbb137c479cb6a282f138cbaed221f15cdc0
Shohinee03 <109723525+Shohinee03@users.noreply.github.com>
2022-09-23 23:13:50 +0530

    Create ass3.py

---
+++ Assignment-8/ass3.py
@@ -0,0 +1,9 @@
+# Write a python script to merge two sorted tuples.
+def merge(list1, list2):
+      
+    merged_list = [(list1[i], list2[i]) for i in range(0, len(list1))]
+    return merged_list
+      
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+print(merge(list1, list2))
